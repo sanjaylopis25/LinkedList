9cdb42eb8c79a8d0c7f53a96a9844c70f4401a3f
sanajylopis25 <sanajylopis25@gmail.com>
2021-12-22 00:50:59 +0530

    First Commit

---
+++ LinkedList1/LinkedList1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinkedList1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Datastructure");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            //list.InsertAtParticularPosition(2,30);
+            list.Display();
+            Console.ReadKey();
+        }
+    }
+}
